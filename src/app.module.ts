b6ec169080380e24179645fbc5bb4f79f89ebff4
WegenerSteven <wegenersteven580@gmail.com>
2025-05-27 16:52:13 +0300

    resource modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,16 @@ import { DatabaseModule } from './database/database.module';
 import { AdminModule } from './modules/admin/admin.module';
 import { DoctorModule } from './modules/doctor/doctor.module';
 import { PatientModule } from './modules/patient/patient.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [DatabaseModule, AdminModule, DoctorModule, PatientModule],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    DatabaseModule,
+    AdminModule,
+    DoctorModule,
+    PatientModule,
+  ],
   controllers: [],
   providers: [],
 })
