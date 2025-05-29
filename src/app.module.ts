a45474be238cfe2b11ac25375efa7cff40647c99
WegenerSteven <wegenersteven580@gmail.com>
2025-05-29 06:58:54 +0300

    update typorm dependency, initiate database connection

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,14 +4,18 @@ import { AdminModule } from './modules/admin/admin.module';
 import { DoctorModule } from './modules/doctor/doctor.module';
 import { PatientModule } from './modules/patient/patient.module';
 import { ConfigModule } from '@nestjs/config';
+import { ProfilesModule } from './modules/profiles/profiles.module';
+import { DepartmentsModule } from './modules/departments/departments.module';
 
 @Module({
   imports: [
-    ConfigModule.forRoot({ isGlobal: true }),
+    ConfigModule.forRoot({ isGlobal: true, envFilePath: '.env' }),
     DatabaseModule,
     AdminModule,
     DoctorModule,
     PatientModule,
+    ProfilesModule,
+    DepartmentsModule,
   ],
   controllers: [],
   providers: [],
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,18 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ConfigService } from '@nestjs/config';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  app.useGlobalPipes(new ValidationPipe());
+
+  const configService = app.get(ConfigService);
+
+  const PORT: number = parseInt(configService.getOrThrow<string>('PORT'), 10);
+  await app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+    console.log('Database connected successfully!');
+  });
 }
 bootstrap();
