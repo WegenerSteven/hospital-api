7f9e8f96eb59ebb4690c921ba06e2f8eed063c2b
WegenerSteven <wegenersteven580@gmail.com>
2025-06-04 06:15:52 +0300

    implement API Documentation

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ConfigService } from '@nestjs/config';
 import { ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 // import { AllExceptionFilter } from 'http-exception.filter';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -9,6 +10,18 @@ async function bootstrap() {
 
   const configService = app.get(ConfigService);
   const PORT: number = parseInt(configService.getOrThrow<string>('PORT'), 10);
+
+  const config = new DocumentBuilder()
+    .setTitle('hospital API')
+    .setDescription('hospital management api')
+    .setVersion('1.0')
+    .addTag('patients')
+    .build();
+
+  const documentFactory = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api/docs', app, documentFactory, {
+    jsonDocumentUrl: 'api-json',
+  });
 
   // const { httpAdapter } = app.getHttpAdapter();
   // app.useGlobalFilters(new AllExceptionFilter(httpAdapter));
