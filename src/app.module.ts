7fb0accd471322f691461dbca155c71ccaec1439
WegenerSteven <wegenersteven580@gmail.com>
2025-06-04 05:48:20 +0300

    import cache managers

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,11 +3,15 @@ import { DatabaseModule } from './database/database.module';
 import { AdminModule } from './modules/admin/admin.module';
 import { DoctorModule } from './modules/doctor/doctor.module';
 import { PatientModule } from './modules/patient/patient.module';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { ProfilesModule } from './modules/profiles/profiles.module';
 import { DepartmentsModule } from './modules/departments/departments.module';
-import { SeedModule } from './seed/seed.module';
+//import { SeedModule } from './seed/seed.module';
 import { LogsModule } from './logs/logs.module';
+import { CachesModule } from './caches/caches.module';
+import { CacheInterceptor, CacheModule } from '@nestjs/cache-manager';
+import { Keyv, createKeyv } from '@keyv/redis';
+import { CacheableMemory } from 'cacheable';
 
 @Module({
   imports: [
@@ -18,10 +22,36 @@ import { LogsModule } from './logs/logs.module';
     PatientModule,
     ProfilesModule,
     DepartmentsModule,
-    SeedModule,
+    //SeedModule,
     LogsModule,
+    CachesModule,
+    //add cache module from cache manager
+    CacheModule.registerAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      isGlobal: true,
+      useFactory: (ConfigService: ConfigService) => {
+        return {
+          // ttl: 60000,
+          stores: [
+            new Keyv({
+              store: new CacheableMemory({
+                ttl: 30000,
+                lruSize: 5000,
+              }),
+            }),
+            createKeyv(ConfigService.getOrThrow<string>('REDIS_URL')),
+          ],
+        };
+      },
+    }),
   ],
   controllers: [],
-  providers: [],
+  providers: [
+    {
+      provide: 'APP_INTERCEPTOR',
+      useClass: CacheInterceptor,
+    },
+  ],
 })
 export class AppModule {}
