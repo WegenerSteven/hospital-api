d46357e9cb45a3a9a605b3d138622be28dcce330
WegenerSteven <wegenersteven580@gmail.com>
2025-06-10 17:44:31 +0300

    remove department entity

--- src/modules/departments/dto/create-department.dto.ts
+++
@@ -1,6 +0,0 @@
-export class CreateDepartmentDto {
-  departmentId?: string;
-  name: string;
-  description?: string;
-  headOfDepartmentId?: string; // Assuming this is a reference to a user or another entity
-}
