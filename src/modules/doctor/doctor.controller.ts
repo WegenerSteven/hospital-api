293f80b411633a4cfcf7b0d1e3f55651901a27fa
WegenerSteven <wegenersteven580@gmail.com>
2025-06-10 18:09:36 +0300

    add guards

--- src/modules/doctor/doctor.controller.ts
+++ src/modules/doctor/doctor.controller.ts
@@ -6,18 +6,24 @@ import {
   Patch,
   Param,
   Delete,
+  UseGuards,
 } from '@nestjs/common';
 import { DoctorService } from './doctor.service';
 import { CreateDoctorDto } from './dto/create-doctor.dto';
 import { UpdateDoctorDto } from './dto/update-doctor.dto';
 import { ApiTags } from '@nestjs/swagger';
+import { AtGuard } from 'src/auth/guards';
+import { Public } from 'src/auth/decorators/public.decorator';
 
 @ApiTags('doctors')
+// @UseGuards(AtGuard)
+@Public()
 @Controller('doctors')
 export class DoctorController {
   constructor(private readonly doctorService: DoctorService) {}
 
   @Post()
+
   create(@Body() createDoctorDto: CreateDoctorDto) {
     return this.doctorService.create(createDoctorDto);
   }
