70c40c99fbc96c8621e44126d3c8e398c7f52e72
WegenerSteven <wegenersteven580@gmail.com>
2025-06-10 17:47:19 +0300

    add rolesguard

--- src/modules/profiles/profiles.module.ts
+++ src/modules/profiles/profiles.module.ts
@@ -4,10 +4,11 @@ import { ProfilesController } from './profiles.controller';
 import { DatabaseModule } from 'src/database/database.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Profile } from './entities/profile.entity';
+import { RolesGuard } from 'src/auth/guards';
 
 @Module({
   imports: [DatabaseModule, TypeOrmModule.forFeature([Profile])],
   controllers: [ProfilesController],
-  providers: [ProfilesService],
+  providers: [ProfilesService, RolesGuard],
 })
 export class ProfilesModule {}
