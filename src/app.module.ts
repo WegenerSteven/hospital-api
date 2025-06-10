06e5207ae383289b76492fcb9097b807253b85d5
WegenerSteven <wegenersteven580@gmail.com>
2025-06-10 17:48:55 +0300

    implement security features (helmet, cors & ratelimitting

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,19 +5,19 @@ import { DoctorModule } from './modules/doctor/doctor.module';
 import { PatientModule } from './modules/patient/patient.module';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { ProfilesModule } from './modules/profiles/profiles.module';
-import { DepartmentsModule } from './modules/departments/departments.module';
 //import { SeedModule } from './seed/seed.module';
 import { LogsModule } from './logs/logs.module';
 import { LoggerMiddleware } from './logger.middleware';
-import { CacheInterceptor, CacheModule } from '@nestjs/cache-manager';
-import { Keyv, createKeyv } from '@keyv/redis';
-import { CacheableMemory } from 'cacheable';
+// import { CacheInterceptor, CacheModule } from '@nestjs/cache-manager';
+// import { Keyv, createKeyv } from '@keyv/redis';
+// import { CacheableMemory } from 'cacheable';
 import { AuthModule } from './auth/auth.module';
 import { AppointmentsModule } from './modules/appointments/appointments.module';
 import { MedicalHistoryModule } from './modules/medical-history/medical-history.module';
 import { APP_GUARD } from '@nestjs/core';
 import { AtGuard } from './auth/guards';
 import { CaslModule } from './casl/casl.module';
+import { ThrottlerGuard, ThrottlerModule } from '@nestjs/throttler';
 
 @Module({
   imports: [
@@ -27,43 +27,57 @@ import { CaslModule } from './casl/casl.module';
     DoctorModule,
     PatientModule,
     ProfilesModule,
-    DepartmentsModule,
     //SeedModule,
     LogsModule,
     //add cache module from cache manager
-    CacheModule.registerAsync({
+    // CacheModule.registerAsync({
+    //   imports: [ConfigModule],
+    //   inject: [ConfigService],
+    //   isGlobal: true,
+    //   useFactory: (ConfigService: ConfigService) => {
+    //     return {
+    //       //ttl: 60000,
+    //       stores: [
+    //         new Keyv({
+    //           store: new CacheableMemory({
+    //             ttl: 30000,
+    //             lruSize: 5000,
+    //           }),
+    //         }),
+    //         createKeyv(ConfigService.getOrThrow<string>('REDIS_URL')),
+    //       ],
+    //     };
+    //   },
+    // }),
+    AuthModule,
+    ThrottlerModule.forRootAsync({
       imports: [ConfigModule],
       inject: [ConfigService],
-      isGlobal: true,
-      useFactory: (ConfigService: ConfigService) => {
-        return {
-          //ttl: 60000,
-          stores: [
-            new Keyv({
-              store: new CacheableMemory({
-                ttl: 30000,
-                lruSize: 5000,
-              }),
-            }),
-            createKeyv(ConfigService.getOrThrow<string>('REDIS_URL')),
-          ],
-        };
-      },
+      useFactory: (config: ConfigService) => [
+        {
+          ttl: config.getOrThrow<number>('THROTTLE_TTL'), //time to live for throttling
+          limit: config.getOrThrow<number>('THROTTLE_LIMIT'), //request limit per id
+          ignoreUserAgents: [/^curl\//, /^PostmanRuntime,\//],
+        },
+      ],
     }),
-    AuthModule,
     AppointmentsModule,
     MedicalHistoryModule,
     CaslModule,
   ],
   controllers: [],
   providers: [
-    {
-      provide: 'APP_INTERCEPTOR',
-      useClass: CacheInterceptor,
-    },
+    // {
+    //   provide: 'APP_INTERCEPTOR',
+    //   useClass: CacheInterceptor,
+    // },
     {
       provide: APP_GUARD,
       useClass: AtGuard, //global guard to protect routes
+    },
+    {
+      provide: APP_GUARD,
+      useClass: ThrottlerGuard,
     },
   ],
 })
