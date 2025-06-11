dbe6ca9560569ba86b196a7e080683d709c7e7e6
WegenerSteven <wegenersteven580@gmail.com>
2025-06-11 10:17:12 +0300

    implement Admin policy and CASL

--- src/modules/admin/admin.controller.ts
+++ src/modules/admin/admin.controller.ts
@@ -6,13 +6,22 @@ import {
   Patch,
   Param,
   Delete,
+  UseGuards,
 } from '@nestjs/common';
 import { AdminService } from './admin.service';
 import { CreateAdminDto } from './dto/create-admin.dto';
 import { UpdateAdminDto } from './dto/update-admin.dto';
 import { ApiTags } from '@nestjs/swagger';
+import { AuthGuard } from '@nestjs/passport';
+import { PoliciesGuard } from 'src/casl/guards/policies.guard';
+import { CreateDoctorDto } from '../doctor/dto/create-doctor.dto';
+import { CheckPolicies } from 'src/casl/decorators/check-policies.decorator';
+import { Action } from 'src/casl/action.enum';
+import { AtGuard, RolesGuard } from 'src/auth/guards';
 
 @ApiTags('admin')
+@UseGuards(AuthGuard('jwt'), PoliciesGuard)
+@CheckPolicies((ability) => ability.can(Action.Manage, 'All'))
 @Controller('admin')
 export class AdminController {
   constructor(private readonly adminService: AdminService) {}
@@ -40,5 +49,11 @@ export class AdminController {
   @Delete(':id')
   remove(@Param('id') id: string) {
     return this.adminService.remove(+id);
+  }
+
+  @Post('doctors')
+  @CheckPolicies((ability) => ability.can(Action.Create, 'Doctor'))
+  createDoctor(@Body() dto: CreateDoctorDto) {
+    return this.adminService.createDoctor(dto);
   }
 }
