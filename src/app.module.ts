064764258336c25cab6f3c1fe41d67f7e673a596
WegenerSteven <wegenersteven580@gmail.com>
2025-06-12 15:52:04 +0300

    enable cache

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,14 +5,14 @@ import { DoctorModule } from './modules/doctor/doctor.module';
 import { PatientModule } from './modules/patient/patient.module';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { ProfilesModule } from './modules/profiles/profiles.module';
+import { AppointmentsModule } from './modules/appointments/appointments.module';
 //import { SeedModule } from './seed/seed.module';
 import { LogsModule } from './logs/logs.module';
 import { LoggerMiddleware } from './logger.middleware';
-// import { CacheInterceptor, CacheModule } from '@nestjs/cache-manager';
-// import { Keyv, createKeyv } from '@keyv/redis';
-// import { CacheableMemory } from 'cacheable';
+import { CacheInterceptor, CacheModule } from '@nestjs/cache-manager';
+import { Keyv, createKeyv } from '@keyv/redis';
+import { CacheableMemory } from 'cacheable';
 import { AuthModule } from './auth/auth.module';
-import { AppointmentsModule } from './modules/appointments/appointments.module';
 import { MedicalHistoryModule } from './modules/medical-history/medical-history.module';
 import { APP_GUARD } from '@nestjs/core';
 import { AtGuard } from './auth/guards';
@@ -27,28 +27,29 @@ import { ThrottlerGuard, ThrottlerModule } from '@nestjs/throttler';
     DoctorModule,
     PatientModule,
     ProfilesModule,
+    AppointmentsModule,
     //SeedModule,
     LogsModule,
     //add cache module from cache manager
-    // CacheModule.registerAsync({
-    //   imports: [ConfigModule],
-    //   inject: [ConfigService],
-    //   isGlobal: true,
-    //   useFactory: (ConfigService: ConfigService) => {
-    //     return {
-    //       //ttl: 60000,
-    //       stores: [
-    //         new Keyv({
-    //           store: new CacheableMemory({
-    //             ttl: 30000,
-    //             lruSize: 5000,
-    //           }),
-    //         }),
-    //         createKeyv(ConfigService.getOrThrow<string>('REDIS_URL')),
-    //       ],
-    //     };
-    //   },
-    // }),
+    CacheModule.registerAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      isGlobal: true,
+      useFactory: (ConfigService: ConfigService) => {
+        return {
+          ttl: 6000,
+          stores: [
+            new Keyv({
+              store: new CacheableMemory({
+                ttl: 30000,
+                lruSize: 5000,
+              }),
+            }),
+            createKeyv(ConfigService.getOrThrow<string>('REDIS_URL')),
+          ],
+        };
+      },
+    }),
     AuthModule,
     ThrottlerModule.forRootAsync({
       imports: [ConfigModule],
@@ -67,10 +68,10 @@ import { ThrottlerGuard, ThrottlerModule } from '@nestjs/throttler';
   ],
   controllers: [],
   providers: [
-    // {
-    //   provide: 'APP_INTERCEPTOR',
-    //   useClass: CacheInterceptor,
-    // },
+    {
+      provide: 'APP_INTERCEPTOR',
+      useClass: CacheInterceptor,
+    },
     {
       provide: APP_GUARD,
       useClass: AtGuard, //global guard to protect routes
