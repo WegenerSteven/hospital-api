239363604c3e490d1d19eabaa55c88dee1c272e2
WegenerSteven <wegenersteven580@gmail.com>
2025-06-12 15:57:03 +0300

    implement role base authorization

--- src/modules/doctor/doctor.controller.ts
+++ src/modules/doctor/doctor.controller.ts
@@ -11,37 +11,43 @@ import {
 import { DoctorService } from './doctor.service';
 import { CreateDoctorDto } from './dto/create-doctor.dto';
 import { UpdateDoctorDto } from './dto/update-doctor.dto';
-import { ApiTags } from '@nestjs/swagger';
-import { AtGuard } from 'src/auth/guards';
-import { Public } from 'src/auth/decorators/public.decorator';
+import { ApiBearerAuth, ApiTags } from '@nestjs/swagger';
+import { Roles } from 'src/auth/decorators/role.decorator';
+import { AtGuard, RolesGuard } from 'src/auth/guards';
+import { Role } from '../profiles/entities/profile.entity';
 
 @ApiTags('doctors')
-// @UseGuards(AtGuard)
-@Public()
+@ApiBearerAuth()
+@UseGuards(RolesGuard, AtGuard)
 @Controller('doctors')
 export class DoctorController {
   constructor(private readonly doctorService: DoctorService) {}
 
+  @Roles(Role.ADMIN, Role.DOCTOR) // Assuming you have a Roles decorator // You can add guards here if needed, e.g., AuthGuard('jwt')
   @Post()
   create(@Body() createDoctorDto: CreateDoctorDto) {
     return this.doctorService.create(createDoctorDto);
   }
 
+  @Roles(Role.ADMIN, Role.DOCTOR)
   @Get()
   findAll() {
     return this.doctorService.findAll();
   }
 
+  @Roles(Role.ADMIN, Role.DOCTOR)
   @Get(':id')
   findOne(@Param('id') id: string) {
     return this.doctorService.findOne(+id);
   }
 
+  @Roles(Role.ADMIN, Role.DOCTOR)
   @Patch(':id')
   update(@Param('id') id: string, @Body() updateDoctorDto: UpdateDoctorDto) {
     return this.doctorService.update(+id, updateDoctorDto);
   }
 
+  @Roles(Role.ADMIN)
   @Delete(':id')
   remove(@Param('id') id: string) {
     return this.doctorService.remove(+id);
--- src/modules/doctor/doctor.module.ts
+++ src/modules/doctor/doctor.module.ts
@@ -3,9 +3,10 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { Doctor } from './entities/doctor.entity';
 import { DoctorService } from './doctor.service';
 import { DoctorController } from './doctor.controller';
+import { Profile } from '../profiles/entities/profile.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Doctor])],
+  imports: [TypeOrmModule.forFeature([Doctor, Profile])],
   controllers: [DoctorController],
   providers: [DoctorService],
 })
