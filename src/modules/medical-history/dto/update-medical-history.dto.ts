3d21945bae807fa9b85c518c48e299cebbd92006
WegenerSteven <wegenersteven580@gmail.com>
2025-06-13 05:10:17 +0300

    refactor folder  structure

--- src/modules/medical-history/dto/update-medical-history.dto.ts
+++
@@ -1,6 +0,0 @@
-import { PartialType } from '@nestjs/swagger';
-import { CreateMedicalHistoryDto } from './create-medical-history.dto';
-
-export class UpdateMedicalHistoryDto extends PartialType(
-  CreateMedicalHistoryDto,
-) {}
--- src/modules/medical-history/medical-history.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { MedicalHistoryService } from './medical-history.service';
-import { MedicalHistoryController } from './medical-history.controller';
-
-@Module({
-  controllers: [MedicalHistoryController],
-  providers: [MedicalHistoryService],
-})
-export class MedicalHistoryModule {}
